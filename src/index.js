f84aaf8238a408d43186fefe89338a0000120d94
brad-bartlett <bradbartlett222@gmail.com>
2019-09-12 18:07:47 -0400

    more

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const parkUl = document.getElementById("list")
 
 
 const ADVCONT = document.querySelector('#adventures-container')
+const ADV = document.getElementById('adventures')
 
     document.addEventListener("DOMContentLoaded", ()=>{
       
       console.log('dom loaded')
       fetchParks()
-      addEventListener()
+      addEventListenerParks()
   
-
     }
     )
   
@@ -44,22 +44,25 @@ const ADVCONT = document.querySelector('#adventures-container')
   }
 
 
-  function addEventListener() {
+  function addEventListenerParks() {
     parkUl.addEventListener('click', function(event) {
       fetchParkDetails(event.target)
     }
     )
+
+
   }
 
-
+  
+  
   function fetchParkDetails(singlePark) {
     const id = singlePark.dataset.id
     fetch('http://localhost:3000/parks/' + id)
-      .then(resp => resp.json())
-      .then(park => {
-        renderParkDetails(park)
-      }
-      )
+    .then(resp => resp.json())
+    .then(park => {
+      renderParkDetails(park)
+    }
+    )
   }
   
   
@@ -78,7 +81,7 @@ const ADVCONT = document.querySelector('#adventures-container')
     ADVCONT.append(parkName)
     ADVCONT.append(parkState)
     ADVCONT.append(parkImage)
-
+    
     renderForm()
     
   }
@@ -86,12 +89,12 @@ const ADVCONT = document.querySelector('#adventures-container')
   function clearDiv() {
     var element = document.getElementById("adventures-container")
     while (element.firstChild) { 
-        element.removeChild(element.firstChild)
+      element.removeChild(element.firstChild)
+    }
   }
-}
   
-
-
+  
+  
   function renderForm() {
     
     const advForm = document.createElement("form")
@@ -100,42 +103,72 @@ const ADVCONT = document.querySelector('#adventures-container')
     const dateInput = document.createElement("input")
     dateInput.setAttribute('type', "date")
     dateInput.setAttribute('placeholder', "MM/DD/YYYY")
-
+    
     const snippetInput = document.createElement("input")
     snippetInput.setAttribute('type', 'string')
     snippetInput.setAttribute('placeholder', "How was your adventure?")
-
+    
     const ratingInput = document.createElement("input")
     ratingInput.setAttribute('type', 'integer')
     ratingInput.setAttribute('placeholder', '1-10')
-
+    
     const submitBtn = document.createElement('input')
-    submitBtn.className = ('submitBtn')
+    submitBtn.id = ('submitBtn')
     submitBtn.setAttribute('type', 'submit')
     submitBtn.setAttribute('value', 'Submit')
-
+    
     advForm.appendChild(dateInput)
     advForm.appendChild(snippetInput)
     advForm.appendChild(ratingInput)
     advForm.appendChild(submitBtn)
-
+    
     document.getElementById("adventures-container").appendChild(advForm)
-  
+    
+    addEventListenerSubmit()
+    
     
   }
+  
+  function addEventListenerSubmit() {
+    const submitBtn = document.getElementById('submitBtn')
+    submitBtn.addEventListener('click', function(event) {
+      event.preventDefault()
+      postAdventure(event.target,submitBtn.parentNode[0].value,submitBtn.parentNode[1].value,submitBtn.parentNode[2].value )
+    }
+    )
+  }
+  
+  function postAdventure(button,date, snippet, rating) {
 
-  // <form id=''>
-  //   <input type='text' name='username' />
-  //   <input type='submit' name='submit' value='submit' />
-  // </form>
+    let adventureObj = {
+      method: "POST",
+      headers: {"Content-Type": "application/json", 
+      "Accept": "application/json"},
+      body: JSON.stringify({
+        date: date,
+        snippet: snippet,
+        rating:rating
+        
+      })
+          }
+        fetch("http://localhost:3000/adventures", adventureObj)
+      .then(resp =>resp.json())
+      .then(adventure=>{
+        renderAdventure(adventure)
+      })
+  }
+  
+  // function renderAdventure(adventure) {
+  //   const date = document.createElement("h2")
+  //   const snippet = document.createElement("h3")
+  //   const rating = document.createElement("h2")
 
+  //   date.textContent = adventure.date
+  //   snippet.textContent = adventure.snippet
+  //   rating.textContent = adventure.snippet
 
-/* <form id="adventure_form">
-        <input id="date_input" type="date" name="date" placeholder="MM-DD-YYYY"/>
-        <input id="snippet_input" type="string" name="snippet" placeholder="How was your adventure?"/>
-        <input id="rating_input" type="integer" name="rating" placeholder="1-10"/>
-        <input type="submit" value="Submit"/>
-      </form> */
-
-
+  //   ADV.append(date)
+  //   ADV.append(snippet)
+  //   ADV.append(rating)
+  // }
 

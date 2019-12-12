cce9a1db88522dd50e0ef1cd245c55a1913bddb8
brad-bartlett <bbartlett222@gmail.com>
2019-12-12 13:54:36 +1100

    status codes

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -2,12 +2,12 @@ class AdventuresController < ApplicationController
 
     def index
         @adventures = Adventure.all
-        render json: @adventures
+        render json: @adventures, status: 200
     end
 
     def create
-        @adventure = Adventure.create(date: params["date"], snippet: params["snippet"],rating: params["rating"], park_id: params["park_id"])
-        render json: @adventure
+        @adventure = Adventure.create(date: params["date"], snippet: params["snippet"], rating: params["rating"], park_id: params["park_id"])
+        render json: @adventure, status: 200
     end
 
 
@@ -16,15 +16,15 @@ class AdventuresController < ApplicationController
         render json: @adventure
     end
 
-    def update   
-        @adventure = Adventure.find(params[:id])   
-        render json: @adventure
-    end   
+    # def update   
+    #     @adventure = Adventure.find(params[:id])   
+    #     render json: @adventure
+    # end   
 
     def destroy   
         @adventure = Adventure.find(params[:id])
         @adventure.destroy!
-        render json: @adventure
+        render json: @adventures, status: 200
     end
 
 # private
